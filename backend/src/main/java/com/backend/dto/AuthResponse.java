f4bc4b1684e5abca746b5a2c561e350fb44d7b49
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-08-04 22:55:02 +0200

    feat: Implement login endpoint and create AuthResponse DTO

---
+++ backend/src/main/java/com/backend/dto/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class AuthResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType = "Bearer";
+}
